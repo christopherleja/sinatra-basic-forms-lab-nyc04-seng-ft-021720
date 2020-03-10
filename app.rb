305002618aed4983c71b0106c9bbb37f2a862178
Christopher Leja <christopher.leja@gmail.com>
2020-03-10 18:10:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+<<<<<<< HEAD
     get '/' do
         erb :index
     end
@@ -14,5 +15,19 @@ class App < Sinatra::Base
         @puppy = Puppy.new(params[:name], params[:breed], params[:age])
         erb :display_puppy
     end
+=======
+  get '/' do
+    erb :index
+  end
+  
+  get '/new' do
+    erb :create_puppy
+  end
+  
+  post '/puppy' do
+    @puppy = Puppy.new(params[:name], params[:age], params[:breed])
+    erb :display_puppy
+  end
+>>>>>>> f5d4b9a4d779e108f7e886f6d07f1ca9e9b51608
 
 end
--- models/puppy.rb
+++ models/puppy.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Puppy
   
     attr_accessor :name, :breed, :age
@@ -16,3 +17,24 @@ class Puppy
     end
     
   end
+=======
+class Puppy
+  
+  attr_accessor :name, :age
+  attr_reader :breed
+  
+  @@all = []
+  
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@all.push(self)
+  end
+  
+  def self.all
+    @@all
+  end
+  
+end
+>>>>>>> f5d4b9a4d779e108f7e886f6d07f1ca9e9b51608
